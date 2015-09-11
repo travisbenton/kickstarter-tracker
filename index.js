98a0617c9dedc25dba963c0b5cc5860da03a8061
Travis Benton <tbenton@spredfast.com>
2015-09-10 23:03:29 -0500

    update moment formatting

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 
+var request = require('request');
+var cheerio = require('cheerio');
+var firebase = require('firebase');
+var database = new firebase('https://kickstarter.firebaseio.com/');
+var moment = require('moment');
+var date = moment().utcOffset(-300).format('YYYY-MM-DD HH:mm');
+var url = 'https://www.kickstarter.com/projects/less/less-like-chess-but-less';
+
+request(url, function (error, response, body) {
+  var $ = cheerio.load(body);
+  
+  database.child(date).set($('[data-pledged]').data());
+  // console.log(date);
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));

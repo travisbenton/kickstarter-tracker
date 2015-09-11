c7dc9efe769b1e8a82dae83691de28299be5e421
Travis Benton <tbenton@spredfast.com>
2015-09-11 08:59:22 -0500

    clean up files

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-var express = require('express');
-var app = express();
-
+// just for testing
 var request = require('request');
 var cheerio = require('cheerio');
 var firebase = require('firebase');
-var database = new firebase('https://kickstarter.firebaseio.com/');
 var moment = require('moment');
+var database = new firebase('https://kickstarter.firebaseio.com/');
 var date = moment().unix();
-var url = 'https://www.kickstarter.com/projects/less/less-like-chess-but-less';
 
-request(url, function (error, response, body) {
+request(process.env.url, function (error, response, body) {
   var $ = cheerio.load(body);
   
   database.child(date).set($('[data-pledged]').data());
 });
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-

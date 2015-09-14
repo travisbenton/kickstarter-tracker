debf2d0a37201e0d4b02b27bb9f5f5723ca1c504
Travis Benton <tbenton@spredfast.com>
2015-09-13 19:23:03 -0500

    first pass at front end

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  gulp.src('./public/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/sass/**/*.scss', ['sass']);
+});
---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var static  = require('express-static');
+
+var app = express();
+
+app.use(static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+var server = app.listen(3000, function(){
+    console.log('server is running at %s', server.address().port);
+});
